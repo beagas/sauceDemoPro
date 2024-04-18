c7c2b485a423a15e79b525e1fe93bcad852f52f0
beagas <beatrice.gaskiene@gmail.com>
2024-04-18 15:50:30 +0300

    successfully checked for add to cart function with classes and arrays

--- src/test/java/TestFullCart.java
+++ src/test/java/TestFullCart.java
@@ -7,11 +7,13 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class TestFullCart {
 
-        WebDriver _driver;
+    WebDriver _driver;
 
     @BeforeClass
     public void SetupWebDriver() {
@@ -24,16 +26,41 @@ public class TestFullCart {
         _driver.findElement(By.id("login-button")).click();
     }
 
-   @Test
-    public void testFullCart(){
-       List <WebElement> itemNames = _driver.findElement(By.className("inventory_list")).findElements(By.className("inventory_item_name"));
-       for (WebElement itemName : itemNames) {
-            System.out.print(itemName.getText());
-            System.out.println();
+    @Test
+    public void testFullCart() {
+        List<WebElement> addToCarts = _driver.findElement(By.className("inventory_list")).findElements(By.xpath("//*[text()='Add to cart']"));
+        for (WebElement addToCart : addToCarts) {//all items are put in cart
+            addToCart.click();
         }
-       List <WebElement> addToCarts = _driver.findElement(By.className("inventory_list")).findElements(By.xpath("//*[text()='Add to cart']"));
-       for (WebElement addToCart : addToCarts) {
-          addToCart.click();
-       }
+        _driver.findElement(By.id("shopping_cart_container")).click();//open cart
+        List<String> itemsCart = new ArrayList<>();
+        List<WebElement> elements = _driver.findElement(By.className("cart_list")).findElements(By.className("inventory_item_name"));// all items that are in cart
+        for (WebElement element : elements) {
+            itemsCart.add(element.getText());
+        }
+        _driver.findElement(By.id("react-burger-menu-btn")).click();//open burger
+        try {
+            Thread.sleep(360);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        _driver.findElement(By.id("inventory_sidebar_link")).click();//open all items
+        List<String> itemNames = new ArrayList<>();
+        List<WebElement> inventorys = _driver.findElement(By.className("inventory_list")).findElements(By.className("inventory_item_name"));
+        List<String> succesfullyInCart = new ArrayList<>();
+        for (WebElement inventory : inventorys) {
+            itemNames.add(inventory.getText());
+        }
+        for (String itemName : itemNames) {
+            for (String itemCart : itemsCart) {
+                if (itemCart.equals(itemName)) {
+                    succesfullyInCart.add(itemName);
+                }
+            }
+        }
+        itemNames.removeAll(succesfullyInCart);
+            for (String item : itemNames) {
+                System.out.println(item);
+            }
     }
 }
